dee53b71adba173bde2b372cd4b269134d37cdea
Rehab <Rehab@DESKTOP-EB3R2A1>
2024-09-10 05:26:19 +0300

    using input decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from './dummy_users';
 
 @Component({
   selector: 'app-root',
@@ -11,5 +12,5 @@ import { UserComponent } from "./user/user.component";
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Angular_Practice';
+  users=DUMMY_USERS;
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component , computed, signal} from '@angular/core';
+import { Component , computed, Input, signal} from '@angular/core';
 import { DUMMY_USERS } from '../dummy_users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -10,15 +10,28 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
-user=signal(DUMMY_USERS[randomIndex]);
-imagePath=computed(()=>'assets/users'+this.user().avatar);//will not computed again
+//   //! without using input decorator
+// user=signal(DUMMY_USERS[randomIndex]);
+// imagePath=computed(()=>'assets/users'+this.user().avatar);//will not computed again
+
+// onSelectUser(){
+//   console.log("clicked");
+// }
+
+// get getImagePath(){
+// return "assets/users/"+ this.user().avatar
+// }
+
+//? Using Input()
+@Input() avatar! : string;
+@Input() name! : string;
 
 onSelectUser(){
   console.log("clicked");
 }
 
-get getImagePath(){
-return "assets/users/"+ this.user().avatar
+get ImagePath(){
+  return 'assets/users/'+this.avatar;
 }
 
 }
