edb4d32dca03b3b555a8e6c9d4ebf54aac9b4ed9
Rehab <Rehab@DESKTOP-EB3R2A1>
2024-09-10 23:37:27 +0300

    add new task to spcifec user

--- src/app/new-task/new-task.component.ts
+++ src/app/new-task/new-task.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NewTaskData } from '../task/task.model';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.scss'
 })
 export class NewTaskComponent {
+  @Output() cancel = new EventEmitter()
+  @Output() addTask = new EventEmitter<NewTaskData>()
+  enteredTitle: string = '';
+  enteredSummary: string = '';
+  enteredDueDate: string = '';
 
+  onSubmit() {
+    this.addTask.emit({
+      title: this.enteredTitle,
+      summary: this.enteredSummary,
+      dueDate: this.enteredDueDate
+    })
+  }
+  onCancel() {
+    this.cancel.emit();
+  }
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,52 +1,71 @@
 import { Component, Input } from '@angular/core';
 import { AppComponent } from "../app.component";
 import { TaskComponent } from "../task/task.component";
+import { NewTaskComponent } from "../new-task/new-task.component";
+import { NewTaskData } from '../task/task.model';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [AppComponent, TaskComponent],
+  imports: [AppComponent, TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.scss'
 })
 export class TasksComponent {
-  @Input({required:true}) userId!: string;
+  @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
+
+  isAddingTask: boolean = false;
   tasks = [
     {
       id: "t1",
-      userId:"u1",
+      userId: "u1",
       title: "Master Angular",
       summary: "Learn All The Basics and Advanced Features Of Angular",
       dueDate: "2024-5-18"
     },
     {
       id: "t2",
-      userId:"u3",
+      userId: "u3",
       title: "Master Angular",
       summary: "Learn All The Basics and Advanced Features Of Angular",
       dueDate: "2024-5-18"
     }, {
       id: "t3",
-      userId:"u3",
+      userId: "u3",
       title: "Master Angular",
       summary: "Learn All The Basics and Advanced Features Of Angular",
       dueDate: "2024-5-18"
     }, {
       id: "t4",
-      userId:"u4",
+      userId: "u4",
       title: "Master Angular",
       summary: "Learn All The Basics and Advanced Features Of Angular",
       dueDate: "2024-5-18"
     },
   ]
-onCompleteTask(id:string){
-  this.tasks=this.tasks.filter((task)=>task.id!==id);
-  console.log("task complete number"+ id)
+  onCompleteTask(id: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+    console.log("task complete number" + id)
 
-}
-  get selectedUserTasks()
-  {
-    return this.tasks.filter((task)=>task.userId===this.userId);
+  }
+  onAddNewTask() {
+    this.isAddingTask = true;
+  }
+  onCancelAddTask() {
+    this.isAddingTask = false;
+  }
+  addTask(taskData: NewTaskData) {
+    this.tasks.push({
+      id: new Date().getTime().toString(),
+      userId: this.userId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.dueDate
+    });
+    this.isAddingTask=false;
+  }
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId);
   }
 }
