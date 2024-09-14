512406786a4b771304a6c85ccb685afe643fe36a
Rehab <Rehab@DESKTOP-EB3R2A1>
2024-09-14 03:00:34 +0300

    init investment branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./header/header.component";
-import { UserComponent } from "./user/user.component";
-import { DUMMY_USERS } from './dummy_users';
-import { TasksComponent } from "./tasks/tasks.component";
-import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent,NgIf],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  users = DUMMY_USERS;
-  selectedUserId?: string;
 
-  get getSelectedUserName() {
-    return this.users.find((user) => user.id === this.selectedUserId)!;
-
-  }
-  onSelectUser(id:string){
-    this.selectedUserId=id;
-  }
-  test(id: string) {
-    this.selectedUserId = id;
-    console.log("user number :" + this.selectedUserId);
-  }
 }
