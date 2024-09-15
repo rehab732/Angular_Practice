faae20f4838d445d12ab8f37434628a0c259aa9d
Rehab <Rehab@DESKTOP-EB3R2A1>
2024-09-15 03:18:11 +0300

    creating component

---
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server-status',
+  standalone: true,
+  imports: [],
+  templateUrl: './server-status.component.html',
+  styleUrl: './server-status.component.scss'
+})
+export class ServerStatusComponent {
+
+}
