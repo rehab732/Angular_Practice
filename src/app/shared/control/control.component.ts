463563f1056bd97202a75e42de6d7502e2a12174
Rehab <Rehab@DESKTOP-EB3R2A1>
2024-09-15 06:37:36 +0300

    host element

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,11 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   standalone: true,
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.scss'
+  styleUrl: './control.component.scss',
+  encapsulation:ViewEncapsulation.ShadowDom
 })
 export class ControlComponent {
 @Input() label?:string;
