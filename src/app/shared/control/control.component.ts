a950e1d0f51cc09c7da981bec19d108debb69846
Rehab <Rehab@DESKTOP-EB3R2A1>
2024-09-15 06:02:35 +0300

    detect type of element need to project

---
+++ src/app/shared/control/control.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-control',
+  standalone: true,
+  imports: [],
+  templateUrl: './control.component.html',
+  styleUrl: './control.component.scss'
+})
+export class ControlComponent {
+@Input() label?:string;
+}
