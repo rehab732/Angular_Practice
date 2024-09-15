8d677af5dedccacbfa49f5b4f219e69f44076c81
Rehab <Rehab@DESKTOP-EB3R2A1>
2024-09-15 05:38:58 +0300

    attribute selector

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-button',
+  selector: 'button[appButton]',//attribute selector
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
