a9873d68b0d5fad64e43402e2bb5b0f3d8256982
Rehab <Rehab@DESKTOP-EB3R2A1>
2024-09-18 12:52:19 +0300

    effect function

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, effect, OnInit, signal } from '@angular/core';
 import { DashboardItemComponent } from "../dashboard-item/dashboard-item.component";
 
 @Component({
@@ -8,7 +8,23 @@ import { DashboardItemComponent } from "../dashboard-item/dashboard-item.compone
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.scss'
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
-
+export class ServerStatusComponent implements OnInit {
+  currentStatus = signal<'online' | 'offline' | 'unKnown'>('online');
+  constructor(){
+    effect(()=>{
+      console.log(this.currentStatus())
+    });
+  }
+  ngOnInit(): void {
+    const interval = setInterval(() => {
+      const ren = Math.random();
+      if (ren < 0.5) {
+        this.currentStatus.set('online');
+      }else if(ren<0.9){
+        this.currentStatus.set('offline');
+      }else{
+        this.currentStatus.set('unKnown');
+      }
+    },5000);
+  }
 }
