340e5aef5d5b585d9b5b1432af2efa1b5c912bc0
Rehab <Rehab@DESKTOP-EB3R2A1>
2024-09-20 20:15:35 +0300

    structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+
+import { AuthComponent } from './auth/auth.component';
+import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.css',
+  imports: [AuthComponent, LearningResourcesComponent],
 })
-export class AppComponent {
-
-}
+export class AppComponent {}
