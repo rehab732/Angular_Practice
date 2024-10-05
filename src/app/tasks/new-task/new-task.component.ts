b70447e0522556c737ae6254a8eb31a784f7c196
Rehab <Rehab@DESKTOP-EB3R2A1>
2024-10-05 05:21:13 +0300

    task service and using signals

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,5 +1,6 @@
-import { Component, ElementRef, viewChild } from '@angular/core';
+import { Component, ElementRef, inject, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-new-task',
@@ -9,9 +10,11 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
+  private taskService=inject(TasksService)
   private formEl = viewChild<ElementRef<HTMLFormElement>>('form');
 
   onAddTask(title: string, description: string) {
+    this.taskService.addNewTask({title,description});
     this.formEl()?.nativeElement.reset();
   }
 }
